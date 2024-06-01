89657e58883f047e565f9ae96b3d7119498f0a52
Monira Azad <monira.azad07@gmail.com>
2024-06-01 23:23:16 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Project Title: Automate a demo fintech API by postman & newman
+
+## Project Summary: In this project, I have created a flow where admin can login and create new users, perform several CRUD actions for the users. Also the users can perform several transactions like send money, deposit, withdraw, payment etc. and check own balance and statement
+
+## Prerequisites:
+
+- NodeJS (Latest LTS)
+- Newman
+- newman-html-extra
+
+## How to run?
+
+### Execute following commands:
+
+- `git clone <repo_url>`
+- `npm i`
+- add the .env file (on request)
+- `node report.js` or
+- `npm test`
+
+## Output:
+
+![image](https://ibb.co/PYz18YB)
---
+++ report.js
@@ -0,0 +1,17 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    // collection:`https://api.postman.com/collections/1844288-70ede3a9-093f-49ec-9a00-6bc8f9bd2c55?access_key=${process.env.secretKey}`,
+    collection: `https://api.postman.com/collections/23167803-c44e381c-d18e-4996-b6f8-3df127de8d6c?access_key=${process.env.secretKey}`,
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
